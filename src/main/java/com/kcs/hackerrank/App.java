0b77ec0659bd6590a4a9b9fdd87c53013bf5edc2
kcshettar <kcshettar@gmail.com>
2020-11-06 09:35:02 -0500

    skeleton code commit

---
+++ src/main/java/com/kcs/hackerrank/App.java
@@ -0,0 +1,13 @@
+package com.kcs.hackerrank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
